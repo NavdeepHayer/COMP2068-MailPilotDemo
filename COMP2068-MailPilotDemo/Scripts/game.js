ace54ba66f3c24fbe26579122e933ec755d6698b
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-03-05 11:46:22 -0500

    Cleaned project - added assets

--- COMP2068-MailPilotDemo/Scripts/game.js
+++ COMP2068-MailPilotDemo/Scripts/game.js
@@ -1,32 +1,18 @@
-﻿var canvas;
-var stage: createjs.Stage;
-
+var canvas;
+var stage;
 // Game Objects 
-
-
 function init() {
     canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas);
     stage.enableMouseOver(20); // Enable mouse events
     createjs.Ticker.setFPS(60); // 60 frames per second
     createjs.Ticker.addEventListener("tick", gameLoop);
-
     main();
 }
-
 function gameLoop() {
-
-
-
     stage.update(); // Refreshes our stage
 }
-
-
-
-
-
 // Our Game Kicks off in here
 function main() {
-
-    
 }
+//# sourceMappingURL=game.js.map
