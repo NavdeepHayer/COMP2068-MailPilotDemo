4fd546a88065efbb419ee8f2969589fa50861237
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-03-05 11:58:51 -0500

    Added Preloader

--- COMP2068-MailPilotDemo/Scripts/game.js
+++ COMP2068-MailPilotDemo/Scripts/game.js
@@ -1,6 +1,19 @@
 var canvas;
 var stage;
+var assetLoader;
+var manifest = [
+    { id: "cloud", src: "assets/images/cloud.png" },
+    { id: "island", src: "assets/images/island.png" },
+    { id: "ocean", src: "assets/images/ocean.gif" },
+    { id: "plane", src: "assets/images/plane.png" }
+];
 // Game Objects 
+function preload() {
+    assetLoader = new createjs.LoadQueue();
+    assetLoader.installPlugin(createjs.Sound);
+    assetLoader.on("complete", init, this);
+    assetLoader.loadManifest(manifest);
+}
 function init() {
     canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas);
@@ -14,5 +27,6 @@ function gameLoop() {
 }
 // Our Game Kicks off in here
 function main() {
+    console.log("Getting our game started...");
 }
 //# sourceMappingURL=game.js.map
--- COMP2068-MailPilotDemo/Scripts/game.ts
+++ COMP2068-MailPilotDemo/Scripts/game.ts
@@ -1,7 +1,23 @@
 ﻿var canvas;
 var stage: createjs.Stage;
+var assetLoader: createjs.LoadQueue;
+
+var manifest = [
+    { id: "cloud", src: "assets/images/cloud.png" },
+    { id: "island", src: "assets/images/island.png" },
+    { id: "ocean", src: "assets/images/ocean.gif" },
+    { id: "plane", src: "assets/images/plane.png" }
+];
 
 // Game Objects 
+
+function preload() {
+
+    assetLoader = new createjs.LoadQueue();
+    assetLoader.installPlugin(createjs.Sound);
+    assetLoader.on("complete", init, this);
+    assetLoader.loadManifest(manifest);
+}
 
 
 function init() {
@@ -27,6 +43,8 @@ function gameLoop() {
 
 // Our Game Kicks off in here
 function main() {
+
+    console.log("Getting our game started...");
 
     
 }
